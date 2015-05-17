365fa2ff3077cb99c1ee86cd395eecd7593fecd2
megafics <master2030@mail.ru>
2015-05-17 21:22:27 +0600

    Initial commit

---
+++ src/matrix/MatrixMultiply.java
@@ -0,0 +1,12 @@
+package matrix;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by admin on 17.05.2015.
+ */
+public class MatrixMultiply {
+    public static void main(String[] args) {
+        System.out.println("Привет Андрей");
+    }
+}
